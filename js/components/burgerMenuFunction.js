4bef86a7093b3b9fe6aa2ecff140e0b2dade7696
Simona <simonasablinaite@gmail.com>
2023-09-17 15:07:54 +0300

    Accordion

--- js/components/burgerMenuFunction.js
+++ js/components/burgerMenuFunction.js
@@ -2,18 +2,19 @@
 const burgerMenuDOM = document.querySelector('.burger-menu');
 const linksDOM = document.getElementById('links');
 
-burgerMenuDOM.addEventListener('click', () => {
 
-   console.log('veikia');
-   function burgerMenuFunction() {
-      if (linksDOM.style.display === 'block') {
-         linksDOM.style.display = "none";
-      } else {
-         linksDOM.style.display = 'block'
+
+export function burgerMenuFunction() {
+   burgerMenuDOM.addEventListener('click', () => {
+      {
+         if (linksDOM.style.display === 'block') {
+            linksDOM.style.display = "none";
+         } else {
+            linksDOM.style.display = 'block'
+         }
       }
-   }
 
-   burgerMenuFunction();
-})
+   })
+}
 
 
